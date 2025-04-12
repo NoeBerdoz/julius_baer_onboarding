c15cd6e5b05018aeb2de3451d6764804bb6b808d
dylan <12473240+dlmw@users.noreply.github.com>
2025-04-12 18:45:02 +0200

    Route for POST new-game

--- app.py
+++ app.py
@@ -4,27 +4,20 @@ from flask import Flask
 
 import config
 from dto.requests import GameStartRequestDTO
-from services.extractor import extract_profile
 from services.julius_baer_api_client import JuliusBaerApiClient
-from services.player import Player
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - [%(module)s] - %(message)s')
+jb_client = JuliusBaerApiClient()
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    jb_client = JuliusBaerApiClient()
+@app.route('/new-game', methods=['POST'])
+def new_game():
     game_start_request = GameStartRequestDTO(player_name=config.API_TEAM)
     res = jb_client.start_game(game_start_request)
 
-    result = extract_profile(res.client_data)
+    return res.model_dump_json()
 
-    player = Player()
-    player.play_on_separate_thread()
 
+if __name__ == '__main__':
     app.run()
