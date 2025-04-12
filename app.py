50204c0d3b0ea49abcf3741ad09747b4398c1a2a
NoeBerdoz <berdoznoe@gmail.com>
2025-04-12 13:40:59 +0200

    remove dead comment

--- app.py
+++ app.py
@@ -20,9 +20,3 @@ if __name__ == '__main__':
     player.play_on_separate_thread()
 
     app.run()
-
-    # res.session_id
-    # UUID('fde19363-a3d5-432e-8b87-54a6dd54f0dd')
-    # second test UUID('e3d58302-400a-4bc6-9772-ae50de43c9f4')
-    # UUID('f8b2a0a6-d4e0-45e6-900f-8ecb3c28f993')
-    # UUID('f8b2a0a6-d4e0-45e6-900f-8ecb3c28f993')
