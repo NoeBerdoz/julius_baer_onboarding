b1443b8cfa7b83fb32590191fcfdb0412421066b
NoeBerdoz <berdoznoe@gmail.com>
2025-04-12 18:40:50 +0200

    add bootstrap

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,3 +1,8 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
 import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
